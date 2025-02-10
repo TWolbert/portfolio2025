7ce81c95b797383769fd3a827a87e1f078ac9122
Teun Wolbert <teunwolbert@gmail.com>
2025-02-10 14:30:28 +0100

    Fix image

--- app/_components/partials/timeline/NodeJS.tsx
+++ app/_components/partials/timeline/NodeJS.tsx
@@ -8,7 +8,7 @@ export function NodeJS() {
             <p className="text-white/70 mb-2">
                 Quite early in my study I was introduced to NodeJS by a classmate of mine. The way we used it initially was quite stupid.
             </p>
-            <Image className="w-full mb-2 rounded-md" src="/BeeMovie.png" width={0} height={0} alt={alt} />
+            <Image className="w-full mb-2 rounded-md" unoptimized src="/BeeMovie.png" width={0} height={0} alt={alt} />
             <p className="text-sm italic text-white/40 mb-2">{alt}</p>
             <p className="text-white/70 mb-2">
                 We used NodeJS to read a file and then send the contents of that file to an email address. The file we read was the Bee movie script. It was a fun little project and I learned a lot about how to use NodeJS.
