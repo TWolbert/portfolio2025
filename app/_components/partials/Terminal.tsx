01af4753b5be1b349cb340b53a2083497ca3956e
Teun Wolbert <teunwolbert@outlook.com>
2025-02-10 19:20:42 +0100

    add terminal

---
+++ app/_components/partials/Terminal.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { useState } from "react";
+import { Fira_Code } from "next/font/google";
+import { HeartFill } from "react-bootstrap-icons";
+
+import Link from "next/link";
+import { IconButton } from "../IconButton";
+import { toast, ToastContainer } from "react-toastify";
+
+const firaCode = Fira_Code({
+    variable: "--font-fira-code",
+    subsets: ["latin"],
+});
+
+
+export default function Terminal() {
+    const [inputValue, setInputValue] = useState("For the love of it_");
+    const [terminalMode, setTerminalMode] = useState(false);
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    }
+
+    const handeEnter = () => {
+        if (inputValue === 'start' && !terminalMode) {
+            setTerminalMode(true)
+            toast.success('Starting up...')
+        }
+        else if (inputValue === 'exit' && terminalMode) {
+            setTerminalMode(false)
+            toast.success('Shutting down...')
+            setInputValue('For the love of it_')
+            return;
+        }
+        else {
+            toast.error('Command not found')
+        }
+
+        setInputValue('')
+    }
+
+
+    return (
+        <div className="h-96 xl:w-full flex items-center relative justify-center overflow-hidden lg:bg-black rounded-xl lg:border-2 lg:border-purple-500 flex-col">
+            <ToastContainer className='absolute top-2 right-2' />
+            <div className="mb-3 flex gap-4 items-center justify-center">
+                <p className="font-bold text-2xl lg:text-3xl text-purple-500 ">{'>'}</p>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => onChange(e)}
+                    // On enter press
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            handeEnter()
+                        }
+                    }}
+                    className={` ${firaCode.className} font-bold text-2xl lg:text-3xl text-purple-500 bg-transparent border-none focus:outline-none`}
+                />
+            </div>
+            {!terminalMode && <>
+                <p className=" text-white/70 text-center max-w-md">
+                    Hi, I&apos;m Teun Wolbert. I&apos;m a software engineer from the Netherlands, I specialize in web development and design
+                </p>
+                <div className="mt-5 lg:p-0 p-2">
+                    <Link href="/projects" className="max-w-md">
+                        <IconButton icon={<HeartFill />}>Sounds cool, show me what you&apos;ve made!</IconButton>
+                    </Link>
+                </div>
+            </>
+            }
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
-import { HeartFill, LightbulbFill } from "react-bootstrap-icons";
-import { IconButton } from "./_components/IconButton";
-import { Fira_Code } from "next/font/google";
-import Link from "next/link";
+import { LightbulbFill } from "react-bootstrap-icons";
 import { HorizontalTimeline, timelineItems } from "./_components/partials/Timeline";
+import Terminal from "./_components/partials/Terminal";
 
-const firaCode = Fira_Code({
-  variable: "--font-fira-code",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
     <div className=" lg:px-0 px-4">
-      <div className="h-96 xl:w-full flex items-center justify-center lg:bg-black rounded-xl lg:border-2 lg:border-purple-500 flex-col">
-        <h1 className={` ${firaCode.className} font-bold text-2xl lg:text-3xl mb-3 text-purple-500`}>{'>'} For the love of it<span className=" animate-pulse">_</span></h1>
-        <p className=" text-white/70 text-center max-w-md">
-          Hi, I&apos;m Teun Wolbert. I&apos;m a software engineer from the Netherlands, I specialize in web development and design
-        </p>
-        <div className="mt-5 lg:p-0 p-2">
-          <Link href="/projects" className="max-w-md">
-            <IconButton icon={<HeartFill />}>Sounds cool, show me what you&apos;ve made!</IconButton>
-          </Link>
-        </div>
-      </div>
+      <Terminal />
 
       <div className=" mt-5">
         <h1 className=" font-bold text-3xl">So what am I good at?</h1>
