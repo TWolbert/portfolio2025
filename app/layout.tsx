a2bb0093e33dbd0135572f11f75f25ab134309a3
Teun Wolbert <teunwolbert@gmail.com>
2025-02-11 09:29:33 +0100

    remove unused Log component import from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./_components/partials/Navbar";
-import Log from "./_components/partials/Log";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
