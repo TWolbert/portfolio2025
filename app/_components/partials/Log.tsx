80a8117c32c721d84a0b1db589d75e0a274bb409
Teun Wolbert <teunwolbert@gmail.com>
2025-02-11 09:43:16 +0100

    re-enable logs

--- app/_components/partials/Log.tsx
+++ app/_components/partials/Log.tsx
@@ -5,8 +5,6 @@ import { headers } from 'next/headers';
 export const dynamic = 'force-dynamic';
 
 export default async function Log() {
-  return;
-
   const headersList = await headers();
   const userAgent = headersList.get('user-agent') || 'unknown';
   
